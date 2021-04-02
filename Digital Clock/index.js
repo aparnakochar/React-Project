e3478c7cbc3485dfa95f286294f8ac937ae9b1ae
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-02 23:51:21 +0530

    first commit

---
+++ Digital Clock/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+ import './index.css';
+import App from './App.js';
+// import Events from './Events';
+import BasicForm from './BasicForm';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './Navbar';
+import QuickSearch from './QuickSearch';
+
+ReactDOM.render(
+   <App />,
+ //<Events />,
+ //<BasicForm />,
+//  <>
+//  <BrowserRouter>
+//    <Navbar />
+//  </BrowserRouter>
+//  </>,
+//<QuickSearch />,
+    document.getElementById('root')
+  );
+  
