9502d27ee311d484363e675629fa3d8527e31dfa
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-04 23:34:28 +0530

    first commit

---
+++ On Click Events/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Events from './Events';
+
+
+ReactDOM.render(
+
+ <Events />,
+    document.getElementById('root')
+  );
+  
