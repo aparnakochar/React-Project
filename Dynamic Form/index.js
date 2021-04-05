36c6cdbd4f43b8bbfdbe0db100847ac144b2ddd3
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-05 23:39:17 +0530

    first commit

---
+++ Dynamic Form/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import BasicForm from './BasicForm';
+
+
+ReactDOM.render(
+
+ <BasicForm />,
+
+    document.getElementById('root')
+  );
