22302b9d39d1453393a487f5a50e29acbcc56229
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-08 23:31:12 +0530

    first commit

---
+++ Quick Search/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import QuickSearch from './QuickSearch';
+
+ReactDOM.render(
+
+<QuickSearch />,
+    document.getElementById('root')
+  );
