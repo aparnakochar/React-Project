72d017c6bef0ea538579813e2d78cecc108caed4
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-10 23:24:33 +0530

    first commit

---
+++ Top Netflix Series(Cards)/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata=[
+    {
+        sname:"dark",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A netflix Original seriess",
+        links:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        sname:"Emily in Paris",
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BYTFjZjQzZDgtOWEyNy00YmY1LTgyYjQtMTBlODUxZTBiZWRkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+        title:"A netflix Original series",
+        links:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        sname:"Friends",
+        imgsrc:"https://bgr.com/wp-content/uploads/2019/09/friends-tv-show.jpg?quality=70&strip=all&w=782",
+        title:"A netflix Original series",
+        links:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        sname:"Money Heist",
+        imgsrc:"https://specials-images.forbesimg.com/imageserve/5d30c8e5eab9270008f9bd2a/960x0.jpg?fit=scale",
+        title:"A netflix Original series",
+        links:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        sname:"Stranger Things",
+        imgsrc:"https://m.hindustantimes.com/rf/image_size_640x362/HT/p2/2016/07/11/Pictures/_afa6f3e2-471c-11e6-90e0-482a513bad8b.png",
+        title:"A netflix Original series",
+        links:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    // {
+    //     sname:"dark",
+    //     imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+    //     title:"A netflix Original series",
+    //     links:"https://www.netflix.com/in/title/80990668?source=35"
+    // },
+]
+export default Sdata;
