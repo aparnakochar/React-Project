ef9c5560e985abda100d2e382b4568865d9ddf9c
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-15 22:49:13 +0530

    first commit

---
+++ Complete Website/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Common from './Common';
+const About = () => {
+  return (
+    <>
+      <Common 
+      name='Welcome to about page'
+      imgsrc={'https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bGlicmFyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'}
+      visit='/contact'
+      btname="contact now"
+      />
+    </>
+  );
+}
+export default About;
