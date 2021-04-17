8a12efea423bbecb44868324fc6e21b5035e555a
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-17 23:42:03 +0530

    first commit

---
+++ Complete Website/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer=()=>{
+    return(
+        <footer className='bg-light text-center navbar fixed-bottom'>
+            <p>
+                ©️ 2021 All Rights Reserved | Terms and Condition
+            </p>
+        </footer>
+    )
+}
+export default Footer;
