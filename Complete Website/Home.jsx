e81fcd447a560b74ae687f2f948fda2895095e43
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2021-04-17 23:42:30 +0530

    first commit

---
+++ Complete Website/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Common from './Common';
+const Home = () => {
+  return (
+   <>
+    <Common 
+      name='Welcome to home page'
+      imgsrc={'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/08/29/724331-c53f73e39737fc501743e16c05aad85c.jpg'}
+      visit='/service'
+      btname="get started"
+      />
+    </>
+  );
+}
+export default Home;
